bbdb4b16fc88557b468287a02e87b674413e2bf7
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-11-13 01:42:27 +0530

    learning about development process of webpack

--- 02-(Micro-Frontends)/M-02(Module Bundler)/Micro-Frontend Bundlers/01-Webpack/02-Documentation/01-Guides/06-Development/practice/src/index.js
+++ 02-(Micro-Frontends)/M-02(Module Bundler)/Micro-Frontend Bundlers/01-Webpack/02-Documentation/01-Guides/06-Development/practice/src/index.js
@@ -1,1 +1,4 @@
+import print from "./print";
+
 console.log("This is development guide");
+print("index.js")
---
+++ 02-(Micro-Frontends)/M-02(Module Bundler)/Micro-Frontend Bundlers/01-Webpack/02-Documentation/01-Guides/06-Development/practice/src/print.js
@@ -0,0 +1,3 @@
+export default function print(filename){
+    console.log("Print is called from", filename)
+}
