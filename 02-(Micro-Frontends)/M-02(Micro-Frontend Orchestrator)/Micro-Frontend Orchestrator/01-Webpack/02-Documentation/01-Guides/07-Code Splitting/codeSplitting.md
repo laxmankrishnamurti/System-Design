1c0a3f7ea8a01ec29fc6363b925d2d9505984603
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-11-19 18:21:33 +0530

    webpack initialization

--- 02-(Micro-Frontends)/M-02(Micro-Frontend Orchestrator)/Micro-Frontend Orchestrator/01-Webpack/02-Documentation/01-Guides/07-Code Splitting/codeSplitting.md
+++ 02-(Micro-Frontends)/M-02(Micro-Frontend Orchestrator)/Micro-Frontend Orchestrator/01-Webpack/02-Documentation/01-Guides/07-Code Splitting/codeSplitting.md
@@ -1,1 +1,18 @@
 # Code Splitting
+
+This feature allows us to split our code into various bundles which can then be loaded on demand or in parallel.
+
+It is used to achieve:
+
+- smaller bundles
+- control resource load prioritization
+
+It used correctly, can have a major impact on load time.
+
+**There are three general approaches to code splitting available:**
+
+- **Entry Points**
+- **Prevent Duplication**
+- **Dynamic Imports**
+
+## **Entry Points**
---
+++ 02-(Micro-Frontends)/M-02(Micro-Frontend Orchestrator)/Micro-Frontend Orchestrator/01-Webpack/02-Documentation/01-Guides/07-Code Splitting/practice/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path")
+const htmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+    mode: "development",
+    entry: {
+        index: './src/index.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            title: "Code Splitting"
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, './dist'),
+        clean: true
+    },
+    optimization: {
+        runtimeChunk: 'single'
+    }
+}
