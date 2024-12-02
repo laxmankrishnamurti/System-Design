123622b35d8d8d96baad8303475871845e2ecd3e
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-12-03 04:04:44 +0530

    Add sayHello function to file

--- 05-(TypeScript)/Module-01(TypeScript)/Docs/Handbook/Section-01(The Handbook)/04-MoreOnFunction/src/testingCallSignatureInJS.js
+++ 05-(TypeScript)/Module-01(TypeScript)/Docs/Handbook/Section-01(The Handbook)/04-MoreOnFunction/src/testingCallSignatureInJS.js
@@ -1,16 +1,20 @@
-function test(num){
-    let randomNumber = Math.floor(Math.random() * 1e2)
-    console.log("function description :: ",test.description)
-    test.sayHello()
-    console.log(test.result)
-    return num > randomNumber
+function test(num) {
+  let randomNumber = Math.floor(Math.random() * 1e2);
+  console.log("function description :: ", test.description);
+  test.sayHello();
+  console.log(test.result);
+  return num > randomNumber;
 }
 
-test.description = "This is a test function"
+test.description = "This is a test function";
 test.sayHello = () => {
-    console.log(`Hello from ${test.name} function`)
-}
-test.result = "All test cases passed!!"
+  console.log(`Hello from ${test.name} function`);
+};
+test.result = "All test cases passed!!";
 
-let resultVal = test(100000)
-console.log("result", resultVal)
+let resultVal = test(100000);
+console.log("result", resultVal);
+
+function sayHello() {
+  console.log("Hello TypeScript");
+}
