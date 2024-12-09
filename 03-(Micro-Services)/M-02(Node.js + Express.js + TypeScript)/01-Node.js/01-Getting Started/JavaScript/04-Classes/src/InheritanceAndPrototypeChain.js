c4343e524d269ce99eadd45502d356f0099a36c1
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-12-10 02:07:24 +0530

    JavaScript: Classes: Inheritance and the prototype chain

--- 03-(Micro-Services)/M-02(Node.js + Express.js + TypeScript)/01-Node.js/01-Getting Started/JavaScript/04-Classes/src/InheritanceAndPrototypeChain.js
+++ 03-(Micro-Services)/M-02(Node.js + Express.js + TypeScript)/01-Node.js/01-Getting Started/JavaScript/04-Classes/src/InheritanceAndPrototypeChain.js
@@ -1,3 +1,7 @@
+/**
+ * Inheriting Properties
+ */
+
 const proto1 = {
     email: "laxmankrishnamurti@gmail.com"
 }
@@ -9,3 +13,68 @@ const user1 = {
 }
 
 console.log(user1.__proto__)
+
+const user2 = {
+    name: "Harshad Mehta",
+    age: 20,
+    __proto__: {
+        email: "harshadmehta@gmail.com",
+        __proto__: {
+            phone: 9508981101,
+            __proto__: {
+                city: "Patna",
+                Street: "9A",
+                House: "Dharamsheela niwas",
+                State: "Bihar",
+                __proto__: {
+                    pin: 800020
+                }
+            }
+        }
+    }
+}
+
+console.log(user2.name)
+console.log(user2.email)
+console.log(user2.State)
+console.log(user2.pin)
+
+console.log(Object.getPrototypeOf(user2.__proto__.__proto__.__proto__))
+
+// Get all prototypes
+
+function getAllPrototypes(obj){
+    const prototypes = []
+    let currentPrototype = Object.getPrototypeOf(obj)
+
+    while(currentPrototype){
+        prototypes.push(currentPrototype)
+        currentPrototype = Object.getPrototypeOf(currentPrototype)
+    }
+
+    return prototypes
+}
+
+const allPrototypes = getAllPrototypes(user2)
+console.log("All Prototypes", allPrototypes)
+
+/*
+
+All Prototypes 
+
+[
+    { email: 'harshadmehta@gmail.com' },
+    { phone: 9508981101 },
+    {
+      city: 'Patna',
+      Street: '9A',
+      House: 'Dharamsheela niwas',
+      State: 'Bihar'
+    },
+    { pin: 800020 },
+    [Object: null prototype] {}
+]
+
+[Object: null prototype]{} ===> This signifies the end of the prototype chain.
+
+*/  
