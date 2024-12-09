f27f80628fe2065f4bcea869de076c3d93c3efdd
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-12-10 05:16:32 +0530

    DLL & Webpack DLLReferencePlugin done

--- 02-(Micro-Frontends)/M-02(Micro-Frontend Orchestrator)/Micro-Frontend Orchestrator/01-Webpack/02-Documentation/01-Guides/11-Build Performance/dll/src/index.js
+++ 02-(Micro-Frontends)/M-02(Micro-Frontend Orchestrator)/Micro-Frontend Orchestrator/01-Webpack/02-Documentation/01-Guides/11-Build Performance/dll/src/index.js
@@ -1,10 +1,1 @@
-const _ = require("lodash")
-
-function getComponent(){
-    const element = document.createElement("div")
-    element.innerHTML = _.join(["hello", "webpack"], " ")
-
-    return element
-}
-
-document.body.appendChild(getComponent())
+console.log("DLL & DLLREFERENCEPLUGIN is successful");
