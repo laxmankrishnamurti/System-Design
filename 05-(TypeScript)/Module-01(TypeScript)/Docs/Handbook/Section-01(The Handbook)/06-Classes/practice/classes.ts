5822fe80777b8a34c1ca8912affa9461c0623479
laxmankrishnamurti <freelancing.laxman@gmail.com>
2024-12-16 10:55:36 +0530

    TypeScript: Classes: Members

--- 05-(TypeScript)/Module-01(TypeScript)/Docs/Handbook/Section-01(The Handbook)/06-Classes/practice/classes.ts
+++ 05-(TypeScript)/Module-01(TypeScript)/Docs/Handbook/Section-01(The Handbook)/06-Classes/practice/classes.ts
@@ -41,3 +41,29 @@ instance2.name = "Laxman Krishnamurti";
 console.log("instance2 name after initialization", instance2.name);
 
 // instance2.pan = "abcdxyz"; (readonly property)
+
+console.log("CONSTRUCTOR");
+
+class Parent3 {
+  userid: string;
+  name: string;
+  age: number;
+  email: string;
+
+  // part-1 (Constructor overloads = define all possible combinations)
+  constructor(userid: number, name: string, age: string, email: string);
+  constructor(userid: string, name: string, age: number, email: string);
+
+  // part-2 (Constructor implimentation = uses all possible combinations, should adhered overlods)
+  constructor(
+    userid: number | string,
+    name: string,
+    age: string | number,
+    email: string
+  ) {
+    this.userid = typeof userid === "number" ? userid.toString() : userid;
+    this.name = name;
+    this.age = typeof age === "string" ? parseInt(age, 10) : age;
+    this.email = email;
+  }
+}
